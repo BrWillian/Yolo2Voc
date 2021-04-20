d756fae1edc8a8d6ce0fc5800a4c28c046c66aa2
willian <wiliam-m-@hotmail.com>
2021-04-20 01:02:07 -0300

    Trailing whitespace

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import Yolo2Voc
 annotation_path = "~/Pictures/yolo-labels" #where are the dataset annotations.
 source = "Personal Dataset" #contains base dataset name.
 output = "~/Pictures/voc-labels" #where it will be saved to new annotations.
-    
+
 Yolo2Voc.convert(annotations_path=annotation_path, source=source, output=output)
 
 #Get version on Yolo2Voc
