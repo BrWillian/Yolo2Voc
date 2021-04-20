76c1994f3e0cf0cdc64486f1eac798d7634229ea
willian <wiliam-m-@hotmail.com>
2021-04-20 00:56:40 -0300

    updated example.py

--- example.py
+++ example.py
@@ -1,5 +1,17 @@
 import Yolo2Voc
 
+
+annotation_path = "~/Pictures/yolo-labels" #where are the dataset annotations.
+source = "Personal Dataset" #contains base dataset name.
+output = "~/Pictures/voc-labels" #where it will be saved to new annotations.
+    
+Yolo2Voc.convert(annotations_path=annotation_path, source=source, output=output)
+
+#Get version on Yolo2Voc
+print(Yolo2Voc.__version__)
+
+#Get Author Name
 print(Yolo2Voc.__author__)
 
-#Yolo2Voc.convert('D:/willian/veiculos/outros', output='D:/willian/labels-veiculos/teste', source='DNIT')
+#Get Author Contact
+print(Yolo2Voc.__email__)
